625104cb0163d940a4fd68446e6c645646533b68
Eathan O'Connor <37672460+oconew@users.noreply.github.com>
2024-08-16 21:33:51 +0100

    commit message

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages/App.tsx'
-import './style/style.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import App from './pages/App'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,15 +1,30 @@
-import mongoose from 'mongoose'
-
+import { MongoClient } from 'mongodb'
 const connectionString = process.env.ATLAS_URI || ''
+// const client = new MongoClient(connectionString)
+const client = new MongoClient(
+  'mongodb+srv://oconew:Nm4pH6sktFyClcuy@cluster0.uln7zx1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+)
+let conn
+try {
+  conn = await client.connect()
+} catch (e) {
+  console.error(e)
+}
+let db = conn.db('db')
+export default db
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB', error)
-  })
+// import mongoose from 'mongoose'
+
+// const connectionString = process.env.ATLAS_URI || ''
+
+// mongoose
+//   .connect(connectionString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to MongoDB')
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB', error)
+//   })
